8bda914a33086d1cf2cb44034aa838b82b235994
Keith Decker <ksuils@gmail.com>
2019-01-21 21:30:57 -0700

    Visits | Process posts, return results in the desired format

--- visits/visits.controller.js
+++ visits/visits.controller.js
@@ -1,17 +1,41 @@
 const db = require('../config/db');
 
-const getVisits = async (req, res, next) => {
-    const visits = await db.visit.findAndCountAll();
-    return res.json(visits);
+async function findAllVisits() {
+    const results = await db.visit.findAll();
+    const toReturn = {};
+    results.forEach(row => {
+        toReturn[row.url] = row.hits
+    });
+
+    return toReturn;
 }
 
-const postVisits = (req, res, next) => {
-    const newVisits = req.body;
-    
-    return res.json({
-        'status': 'OK',
-        newVisits
+const getVisits = async (req, res, next) => {
+    return res.json(await findAllVisits());
+}
+
+const postVisits = async (req, res, next) => {
+    const newVisits = req.body.map(async (url) => {
+        // TODO process the url here
+        const foundUrl = await db.visit.findOne({
+            where: {
+                url
+            }
+        });
+
+        if (foundUrl) {
+            return foundUrl.increment('hits');
+        }
+
+        return db.visit.create({
+            url,
+            hits: 1
+        });
     });
+
+    await Promise.all(newVisits);
+    
+    return res.json(await findAllVisits());
 }
 
 module.exports = {    
